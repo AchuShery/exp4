627a12131a5334011faa46f0c6fca85401044b8a
AchuShery <162905317+AchuShery@users.noreply.github.com>
2024-03-15 02:12:04 +0530

    num.py

--- num.py
+++ num.py
@@ -1,16 +1,10 @@
+#This program adds two numbers
 
-This program adds two numbers
-
-+ num1 2.5
-+ num25.3
++ num1 = 2.5
++ num2 = 5.3
 
 # Add two numbers
-sum=num1 + num
-# This program adds two numbers
+sum = num1 + num2
 
-num1 = 1.5
-num2 = 6.3
-
-#Add two numbers.
-
-sum = num1 + nu
+#Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, num))
